bbd685b4d4bb9256b4ab255a15370efab69c2b69
Mithunsingh1 <samarsingh9569@gmail.com>
2025-03-29 13:11:57 +0530

    first commit

--- src/main/java/com/example/mongodb/springboot_mongodb/model/Employee.java
+++ src/main/java/com/example/mongodb/springboot_mongodb/model/Employee.java
@@ -3,14 +3,20 @@ package com.example.mongodb.springboot_mongodb.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+
 @Document(collection = "employees")
 public class Employee {
 	@Id
 	private String id;
 	private String name;
 	private String email;
+	
 	private String location;
+	
+	
 }
